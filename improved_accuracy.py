60df2d96403c469d742cfc216a96f4960ac6ca17
bernie-xailient <59948502+bernie-xailient@users.noreply.github.com>
2020-05-18 17:21:03 +1000

    tested and debugged

--- improved_accuracy.py
+++ improved_accuracy.py
@@ -5,32 +5,32 @@ import cv2 as cv
 
 #By default Low resolution DNN for face detector will be loaded.
 #To load the high resolution Face detector please comment the below lines.
-def main()
+def main():
 	detectum = dnn.Detector()
 	THRESHOLD = 0.4 # Value between 0 and 1 for confidence score
 
-	im = cv.imread('../data/beatles.jpg')
+	im = cv.imread('../data/party.jpeg')
 
 	_, bboxes = detectum.process_frame(im, THRESHOLD)
-	
-	
+
+
 	# --- uncomment to utilise post-processing --- #
 	# 1.
 	# Post-processing to remove any bboxes whose coordinates are inside larger bboxes.
-	# remove_inside_bboxes(bboxes) # uncomment me!!!
-	# 2. 
+	# bboxes = remove_inside_bboxes(bboxes) # uncomment me!!!
+	# 2.
 	# Post-processing that combines any two overlapping bboxes into one larger bbox.
-	# combine_bboxes(bboxes) # uncomment me!!!
-	
-	
+	# bboxes = combine_bboxes(bboxes) # uncomment me!!!
+
+
 	# Loop through list (if empty this will be skipped) and overlay green bboxes
 	# Format of bboxes is: xmin, ymin (top left), xmax, ymax (bottom right)
 	for i in bboxes:
 		cv.rectangle(im, (i[0], i[1]), (i[2], i[3]), (0, 255, 0), 3)
 
-	cv.imwrite('../data/beatles_output.jpg', im)
+	cv.imwrite('../data/party_remove_output.jpeg', im)
 
-	
+
 #######################
 ### POST-PROCESSING ###
 #######################
@@ -41,7 +41,7 @@ def remove_inside_bboxes(bboxes):
 	for test_bbox in bboxes:
 		if not smaller_bbox(test_bbox, bboxes):
 			filtered_bboxes.append(test_bbox)
-	bboxes = filtered_bboxes
+	return filtered_bboxes
 
 def smaller_bbox(test_bbox, bboxes):
 	t_xmin = test_bbox[0]
@@ -52,18 +52,18 @@ def smaller_bbox(test_bbox, bboxes):
 	for bbox in bboxes:
 		if list(test_bbox) == list(bbox):
 			continue
-			
+
 		b_xmin = bbox[0]
 		b_ymin = bbox[1]
 		b_xmax = bbox[2]
 		b_ymax = bbox[3]
-		
+
 		# Return True if test_bbox coordinates is inside another bounding box
 		if b_xmin <= t_xmin <= b_xmax and b_xmin <= t_xmax <= b_xmax \
 		and b_ymin <= t_ymin <= b_ymax and b_ymin <= t_ymax <= b_ymax:
 			return True
 	return False
-	
+
 
 # 2. Post-processing that combines any two overlapping bboxes into one larger bbox.
 def combine_bboxes(bboxes):
@@ -71,10 +71,10 @@ def combine_bboxes(bboxes):
 	combined = []
 	loop_again = True
 	match = False
-	
+
 	while(loop_again):
 		loop_again = False
-		
+
 		for bboxA in bboxes:
 			for bboxB in bboxes:
 
@@ -84,38 +84,38 @@ def combine_bboxes(bboxes):
 					ymin = max(bboxA[1], bboxB[1])
 					xmax = min(bboxA[2], bboxB[2])
 					ymax = min(bboxA[3], bboxB[3])
-					
+
 					# compute the area of intersection rectangle
 					interArea = max(0, xmax - xmin) * max(0, ymax - ymin)
-					
+
 					# If intersection > 0, compute coordinates of combined bbox
 					if interArea > 0:
 						xmin = min(bboxA[0], bboxB[0])
 						ymin = min(bboxA[1], bboxB[1])
 						xmax = max(bboxA[2], bboxB[2])
 						ymax = max(bboxA[3], bboxB[3])
-						
+
 						if [xmin,ymin,xmax,ymax] not in combined:
 							combined.append([xmin,ymin,xmax,ymax])
-							
+
 						match = True
 						loop_again = True # We need to loop again to ensure this combined bbox
 								  # is not overlapping with another bbox
-			
+
 			if match == False: # if bboxA doesn't overlap with any other bbox in this current interation
 				if bboxA not in not_matched_bboxes:
 					not_matched_bboxes.append(bboxA)
 			match = False
-		
+
 		bboxes = combined.copy()
 		bboxes.extend(not_matched_bboxes)
 		combined = []
-				
+
 	return bboxes
-	
-	
-	
+
+
+
 if __name__ == "__main__":
 	main()
-	
+
 
