655fc3f69948f18973d244212499d328e4299e56
Sabina <sabina@xailient.com>
2020-06-04 16:08:31 +0545

    Updated code for HTTP post reference implementation.

--- HTTP_post_results.py
+++ HTTP_post_results.py
@@ -88,45 +88,19 @@ def encode(image):
     return base64_message
 
 def main():
-    # initialize the camera and grab a reference to the raw camera capture
-    RES_W = 640 # 1280 # 640 # 256 # 320 # 480 # pixels
-    RES_H = 480 # 720 # 480 # 144 # 240 # 360 # pixels
+    im = cv.imread('../data/beatles.jpg')
+
+    _, bboxes = detectum.process_frame(im, THRESHOLD)
+
+    # Loop through list (if empty this will be skipped) and overlay green bboxes
+    # Format of bboxes is: xmin, ymin (top left), xmax, ymax (bottom right)
+    for i in bboxes:
+        cv.rectangle(im, (i[0], i[1]), (i[2], i[3]), (0, 255, 0), 3)
     
-    camera = PiCamera()
-    camera.resolution = (RES_W, RES_H)
-    camera.framerate = 30 # FPS
-    rawCapture = PiRGBArray(camera, size=(RES_W, RES_H))
+    # If any object detected, send result to API using HTTP post
+    if len(bboxes) > 0:
+        send_results(im, bboxes)
 
-    # allow the camera to warmup
-    time.sleep(0.1)
-
-    # capture frames from the camera
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        # grab the raw NumPy array representing the image, then initialize the timestamp
-        # and occupied/unoccupied text
-        image = frame.array
-        image_cpy = np.copy(image) # Create copy since cant modify orig
-
-        # Return's list of bounding box co-ordinates
-        _, bboxes = detectum.process_frame(image, THRESHOLD)
-
-        # Loop through list (if empty this will be skipped) and overlay green bboxes
-        for i in bboxes:
-            cv.rectangle(image_cpy, (i[0], i[1]), (i[2], i[3]), (0, 255, 0), 3)
-
-        # If any object detected, send result to API using HTTP post
-        if len(bboxes) > 0:
-            send_results(image, bboxes)
-
-        # show the frame
-        cv.imshow("Frame", image_cpy)
-        key = cv.waitKey(1) & 0xFF
-
-        # clear the stream in preparation for the next frame
-        rawCapture.truncate(0)
-
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
+    cv.imwrite('../data/beatles_output.jpg', im)
 
 main()
